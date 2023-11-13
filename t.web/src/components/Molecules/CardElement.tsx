2d5268454872af2d351df644f5102c103b43a2fe
hirotaka42(yuki) <79750434+hirotaka42@users.noreply.github.com>
2023-11-14 04:06:35 +0900

    feat: Add thumbnail display to card element

--- t.web/src/components/Molecules/CardElement.tsx
+++ t.web/src/components/Molecules/CardElement.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Button, Card, CardContent, Typography } from '@mui/material';
+import { Box, Button, Card, CardActionArea, CardMedia, CardContent, Typography } from '@mui/material';
 import { Button_OpenIina } from '@/components/Atoms/Button/OpenIina'
 import { Button_CopyLink } from '@/components/Atoms/Button/CopyLink'
 
@@ -30,18 +30,63 @@ interface CardElementProps {
 
 export const CardElement: React.FC<CardElementProps> = ({ object }) => {
     return (
+      <Box sx={{ maxWidth: 260 }}>
       <Card>
-        <CardContent>
-          <Typography variant="h5" component="div">
+        <CardActionArea>
+          <CardMedia
+            component="img"
+            height="140"
+            image={`https://statics.tver.jp/images/content/thumbnail/episode/large/${object.content.id}.jpg`}
+            alt={object.content.seriesTitle}
+          />
+        </CardActionArea>
+      </Card>
+        <CardContent 
+          sx={{ 
+            marginTop: 0.8,
+            padding: 0,
+            width: '100%',
+            '&.MuiCardContent-root:last-child': {
+              paddingBottom: 0,
+            },
+           }}
+        >
+          <Typography variant="h6" component="div" 
+            sx={{
+              fontSize: '15px',
+              lineHeight: '18px',
+              paddingBottom: '2px',
+              fontWeight: '700',
+              textOverflow: 'ellipsis', 
+              whiteSpace: 'nowrap',
+              overflow: 'hidden',
+              color: '#1f2b3e',
+
+            }}>
             {object.content.seriesTitle}
           </Typography>
-          <Typography variant="body2" color="text.secondary">
+          <Typography variant="body2" color="text.secondary"
+            sx={{
+              fontSize: '13px',
+              lineHeight: '17px',
+              textOverflow: 'ellipsis', 
+              whiteSpace: 'nowrap',
+              overflow: 'hidden',
+              color: '#1f2b3e',
+            }}>
             {object.content.title}
           </Typography>
-          <Button_OpenIina videoLink={object.content.id} />
-          <Button_CopyLink videoLink={object.content.id} />
+          <Box
+            sx={{
+              display: 'flex',
+              justifyContent: 'center',
+              alignItems: 'center',
+            }}
+          >
+            <Button_OpenIina videoLink={object.content.id} />
+            <Button_CopyLink videoLink={object.content.id} />
+          </Box>
         </CardContent>
-      </Card>
-
+      </Box>
     );
   };
